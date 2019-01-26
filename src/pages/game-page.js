10879a0cacc17029ec7eeb159d9bebb08257a94f
Staś Małolepszy <stas@duzodobrze.pl>
2019-01-26 17:51:30 +0100

    SImple floor

--- src/pages/game-page.js
+++ src/pages/game-page.js
@@ -13,11 +13,15 @@ const page3 = ( props ) => {
         <Scene>
             <a-assets>
                 <a-asset-item id="model-car1" src="./car1.glb"></a-asset-item>
+                <a-asset-item id="model-map1" src="./map1.glb"></a-asset-item>
             </a-assets>
             <Camera></Camera>
             <Entity
+                gltf-model="#model-map1"
+                position={{x: 0, y: 0, z: 0}}/>
+            <Entity
                 gltf-model="#model-car1"
-                keyboard-controls={{acceleration: 400, turnSpeed: Math.PI / 2}}
+                keyboard-controls={{acceleration: 400, turnSpeed: Math.PI}}
                 position={{x: 0, y: 0, z: 0}}
                 gamepad-controls={{controller: 0, lookEnabled: false, debug: true}}/>
         </Scene>
