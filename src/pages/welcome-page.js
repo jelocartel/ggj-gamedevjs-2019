4db820b1d3663fa62d9bac089125c0d8249022f1
Wasiak <wasiakj@emea.roche.com>
2019-01-26 20:53:31 +0100

    welcom page

--- src/pages/welcome-page.js
+++ src/pages/welcome-page.js
@@ -7,16 +7,17 @@ import './pages.css';
 const page1 = ( props ) => {
     return (
         <div>
+            <h1>Welcome to Jelonki!</h1>
             <Scene>
-            <a-assets>
-                <a-asset-item id="model-car1blue" src="./car1blue.glb"></a-asset-item>
-            </a-assets>
-            <Camera 
-                position={{x: 6.88, y: 0.96, z: -6.7}}
-                rotation={{x: 3.5, y: 128.5, z: 0}}></Camera>
-            <Entity
-                gltf-model="#model-car1blue"
-                position={{x: 0, y: 0, z: 0}}/>
+                <a-assets>
+                    <a-asset-item id="model-car1blue" src="./car1blue.glb"></a-asset-item>
+                </a-assets>
+                <Camera 
+                    position={{x: 6.88, y: 0.96, z: -6.7}}
+                    rotation={{x: 3.5, y: 128.5, z: 0}}></Camera>
+                <Entity
+                    gltf-model="#model-car1blue"
+                    position={{x: 0, y: 0, z: 0}}/>
             </Scene>
             <button onClick={props.click}>Start</button>
         </div>
