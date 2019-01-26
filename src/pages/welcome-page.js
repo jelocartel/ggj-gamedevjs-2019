f5687c43bdf1628f9b06afe17ce796e96f0306f1
Wasiak <wasiakj@emea.roche.com>
2019-01-26 19:51:18 +0100

    camera on welcome page, cars colors

--- src/pages/welcome-page.js
+++ src/pages/welcome-page.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {Scene, Entity} from "aframe-react";
+import { Camera } from "../entities/camera";
+
 import './pages.css';
 
 const page1 = ( props ) => {
     return (
-        <div className='page'>
-            <h1>Welcome</h1>
+        <div>
+            <Scene>
+            <a-assets>
+                <a-asset-item id="model-car1blue" src="./car1blue.glb"></a-asset-item>
+            </a-assets>
+            <Camera 
+                position={{x: 6.88, y: 0.96, z: -6.7}}
+                rotation={{x: 3.5, y: 128.5, z: 0}}></Camera>
+            <Entity
+                gltf-model="#model-car1blue"
+                position={{x: 0, y: 0, z: 0}}/>
+            </Scene>
             <button onClick={props.click}>Start</button>
         </div>
     )
