573acfca2437837e58f05e31b5f80034e256bddc
Staś Małolepszy <stas@duzodobrze.pl>
2019-01-26 12:30:36 +0100

    Add A-Frame

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "aframe";
+import {Scene, Entity} from "aframe-react";
+
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return <Scene>
+            <Entity
+                geometry={{primitive: "box"}}
+                material={{color: "red"}}
+                position={{x: 0, y: 0, z: -5}}/>
+        </Scene>;
+    }
 }
 
 export default App;
