e1c778f8258a758de192e4f63e9ae08be3cebb0f
wojtekw92 <wwozniak92@gmail.com>
2019-01-27 17:23:52 +0100

    Deploy updatwe

---
+++ dist/precache-manifest.26ac9b216d816f478f41b58cefc7b0cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "040c98ebf1f78a01eacc",
+    "url": "/ggj-gamedevjs-2019/static/js/runtime~main.040c98eb.js"
+  },
+  {
+    "revision": "42a06a51c23b652bf721",
+    "url": "/ggj-gamedevjs-2019/static/js/main.42a06a51.chunk.js"
+  },
+  {
+    "revision": "3b7ac65c05b3940b3b83",
+    "url": "/ggj-gamedevjs-2019/static/js/1.3b7ac65c.chunk.js"
+  },
+  {
+    "revision": "42a06a51c23b652bf721",
+    "url": "/ggj-gamedevjs-2019/static/css/main.c8cedb6c.chunk.css"
+  },
+  {
+    "revision": "4ff18fea105616b212447f98db1d0c13",
+    "url": "/ggj-gamedevjs-2019/index.html"
+  }
+];
