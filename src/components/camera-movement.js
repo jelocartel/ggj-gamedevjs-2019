3d7a28b4cab3dec29ba5289cf8df057a7258c514
wojtekw92 <wwozniak92@gmail.com>
2019-01-27 17:50:40 +0100

    kamera

--- src/components/camera-movement.js
+++ src/components/camera-movement.js
@@ -27,44 +27,11 @@ AFRAME.registerComponent('camera-movment', {
         let cameraZ = (maxDim / 4) / Math.tan(fov * 2);
         // console.log(maxDim)
         // this.el.object3D.position.x = maxDim > 34 ? maxDim*1.5 : 51;
-        // this.el.object3D.position.y = maxDim > 40 ? maxDim*2 : 80;
+        this.el.object3D.position.x = maxDim > 40 ? maxDim*2 : 80;
+        this.el.object3D.position.y = maxDim > 40 ? maxDim*2 : 80;
+        this.el.object3D.position.z = maxDim > 40 ? maxDim*2 : 80;
         // this.el.object3D.position.z = maxDim > 34 ? maxDim*1.5 : 51;
         // this.el.object3D.updateProjectionMatrix();
         // console.log(size);
     }
 });
-
-
-module.exports = {
-    tick() {
-        const ratio = window.innerWidth / window.innerHeight;
-        const cars = document.querySelectorAll('.car');
-        const offset = 1.75;
-        const camera = this.el.children[0].object3D.children[0];
-        // camera.aspect = ratio;
-
-        const boundingBox = new THREE.Box3();
-        const group = new THREE.Group();
-
-        cars.forEach((car) => {
-            group.add(car.object3D.clone());
-        });
-
-        boundingBox.setFromObject(group);
-
-        const center = boundingBox.getCenter();
-        const size = boundingBox.getSize();
-
-        const maxDim = Math.max(size.x, size.y, size.z);
-        const fov = camera.fov * (Math.PI / 180);
-        let cameraZ = (maxDim / 4) / Math.tan(fov * 2);
-
-        // cameraZ *= offset;
-        camera.position.z = cameraZ;
-        camera.updateProjectionMatrix();
-        camera.lookAt(center);
-
-
-
-    }
-}
