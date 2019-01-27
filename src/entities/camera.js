fdb3d17e7e5f1dc3565ec31f2c801bfaa7290312
Staś Małolepszy <stas@duzodobrze.pl>
2019-01-27 10:31:18 +0100

    Add map2 model

--- src/entities/camera.js
+++ src/entities/camera.js
@@ -4,7 +4,7 @@ import {Entity} from "aframe-react";
 
 const Camera = (props) => {
     return (
-        <Entity rotation={{ x: -30, y: 45, z: 0 }} position={{ x: 50, y: 50, z: 50 }} {...props}>
+        <Entity rotation={{ x: -30, y: 45, z: 0 }} position={{ x: 60, y: 60, z: 60 }} {...props}>
             <Entity camera="active: true; fov: 30" data-aframe-default-camera></Entity>
         </Entity>
     );
