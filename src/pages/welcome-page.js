d984b2b5680120e37e41472bf484e65223970070
wojtekw92 <wwozniak92@gmail.com>
2019-01-27 17:56:33 +0100

    Updated first camera

--- src/pages/welcome-page.js
+++ src/pages/welcome-page.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Scene, Entity} from "aframe-react";
-import { Camera } from "../entities/camera";
 
 import './pages.css';
 
@@ -12,9 +11,10 @@ const page1 = ( props ) => {
                 <a-assets>
                     <a-asset-item id="model-car1blue" src="./car1blue.glb"></a-asset-item>
                 </a-assets>
-                <Camera 
-                    position={{x: 6.88, y: 0.96, z: -6.7}}
-                    rotation={{x: 3.5, y: 128.5, z: 0}}></Camera>
+                    <Entity position={{x: 6.88, y: 0.96, z: -6.7}}
+                    rotation={{x: 3.5, y: 128.5, z: 0}}>
+                        <Entity camera="active: true; fov: 30" data-aframe-default-camera near="0.001"></Entity>
+                    </Entity>
                 <Entity
                     gltf-model="#model-car1blue"
                     position={{x: 0, y: 0, z: 0}}/>
